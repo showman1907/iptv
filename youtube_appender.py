3fac187d797235f6740ac38c32d7b7d6ffafb804
showman1907 <gokhantkgm44@gmail.com>
2025-04-03 23:14:00 +0300

    Update youtube_appender.py

--- youtube_appender.py
+++ youtube_appender.py
@@ -31,7 +31,7 @@ for line in lines:
 youtube_section = ["\n# === YOUTUBE ===\n"]
 for video in youtube_videos:
     youtube_section.append(
-        f'#EXTINF:-1 tvg-id="youtube" tvg-name="{video["name"]}" tvg-logo="https://img.youtube.com/vi/{video["id"]}/hqdefault.jpg",{video["name"]}\n'
+        f'#EXTINF:-1 tvg-id="youtube" tvg-name="{video["name"]}" tvg-logo="https://img.youtube.com/vi/{video["id"]}/hqdefault.jpg" group-title="YouTube",{video["name"]}\n'
     )
     youtube_section.append(f'https://www.youtube.com/watch?v={video["id"]}\n')
 
